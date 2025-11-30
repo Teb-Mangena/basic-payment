8f5731020a74b5f9823b067a7ed3830ffc630fbd
Tebatso <tebatsomangena8@gmail.com>
2025-11-30 18:19:01 +0200

    added role restrictions middleware

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,8 @@
+export const requireRole = (roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Forbidden: only assigned roles can perform this action" });
+    }
+    next();
+  };
+};
